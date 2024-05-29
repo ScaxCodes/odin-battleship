56de0403f5ab47ebbb9abfe8ec2f786833ea538b
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-05-29 14:07:42 +0200

    Add ship factory function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import "./normalize.css";
 import "./styles.css";
 
-console.log("Hello World")
+function createShip(size) {
+  return {
+    length: size,
+    hitsCounter: 0,
+    sunken: false,
+    hit: function () {
+      this.hitsCounter++;
+    },
+    isSunk: function () {
+      return this.length <= this.hitsCounter;
+    },
+  };
+}
+
+const ship = createShip(3);
+ship.hit();
+console.log(ship);
+ship.hit();
+console.log(ship.isSunk());
+ship.hit();
+console.log(ship);
+console.log(ship.isSunk());
