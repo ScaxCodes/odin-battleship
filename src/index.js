490de12f9f1ab4adb36bfa91a9e6500fd4b5b6a5
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-05-29 22:00:04 +0200

    Add board factory function

--- src/index.js
+++ src/index.js
@@ -15,11 +15,49 @@ function createShip(size) {
   };
 }
 
-const ship = createShip(3);
-ship.hit();
-console.log(ship);
-ship.hit();
-console.log(ship.isSunk());
-ship.hit();
-console.log(ship);
-console.log(ship.isSunk());
+function createBoard() {
+  return {
+    grid: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    placeShip: function (coordsArray) {
+      const ship = createShip(coordsArray.length);
+      coordsArray.forEach((coord) => {
+        this.grid[coord.x][coord.y] = ship;
+      });
+    },
+  };
+}
+
+function testShipCreation() {
+  const ship = createShip(3);
+  ship.hit();
+  console.log(ship);
+  ship.hit();
+  console.log(ship.isSunk());
+  ship.hit();
+  console.log(ship);
+  console.log(ship.isSunk());
+}
+
+function testBoardCreation() {
+  const board = createBoard();
+  console.log(board.grid);
+
+  const board2 = createBoard();
+  board2.placeShip([
+    { x: 3, y: 5 },
+    { x: 3, y: 6 },
+    { x: 3, y: 7 },
+  ]);
+  console.log(board2.grid);
+}
