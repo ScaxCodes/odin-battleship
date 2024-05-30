9c5c83cadefb0cfb039c250e8223ecb1d7903e26
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-05-30 21:15:42 +0200

    Populate boards with ships, add board structure to DOM

--- src/index.js
+++ src/index.js
@@ -3,6 +3,74 @@ import "./styles.css";
 
 import { createShip, createBoard, createPlayer } from "./factories.js";
 
+const player = createPlayer();
+const enemy = createPlayer("computer");
+
+// Populate each board with ships, adding custom positioning later
+function populateBoardsWithShips() {
+  player.ownBoard.placeShip([
+    { x: 0, y: 0 },
+    { x: 0, y: 1 },
+  ]);
+  player.ownBoard.placeShip([
+    { x: 1, y: 0 },
+    { x: 1, y: 1 },
+    { x: 1, y: 2 },
+  ]);
+  player.ownBoard.placeShip([
+    { x: 2, y: 0 },
+    { x: 2, y: 1 },
+    { x: 2, y: 2 },
+    { x: 2, y: 3 },
+  ]);
+  player.ownBoard.placeShip([
+    { x: 3, y: 0 },
+    { x: 3, y: 1 },
+    { x: 3, y: 2 },
+    { x: 3, y: 3 },
+    { x: 3, y: 4 },
+  ]);
+  enemy.ownBoard.placeShip([
+    { x: 6, y: 0 },
+    { x: 6, y: 1 },
+  ]);
+  enemy.ownBoard.placeShip([
+    { x: 7, y: 0 },
+    { x: 7, y: 1 },
+    { x: 7, y: 2 },
+  ]);
+  enemy.ownBoard.placeShip([
+    { x: 8, y: 0 },
+    { x: 8, y: 1 },
+    { x: 8, y: 2 },
+    { x: 8, y: 3 },
+  ]);
+  enemy.ownBoard.placeShip([
+    { x: 9, y: 0 },
+    { x: 9, y: 1 },
+    { x: 9, y: 2 },
+    { x: 9, y: 3 },
+    { x: 9, y: 4 },
+  ]);
+}
+
+populateBoardsWithShips();
+console.log(player.ownBoard.grid);
+console.log(enemy.ownBoard.grid);
+
+const playerBoard = document.querySelector(".board.player");
+const enemyBoard = document.querySelector(".board.enemy");
+
+for (let i = 0; i < 100; i++) {
+  const singleField = document.createElement("div");
+  singleField.classList.add("single-field");
+  const anotherSingleField = document.createElement("div");
+  anotherSingleField.classList.add("single-field");
+  playerBoard.append(singleField);
+  enemyBoard.append(anotherSingleField);
+}
+
+// Testing
 function testShipCreation() {
   const ship = createShip(3);
   ship.hit();
@@ -50,7 +118,7 @@ function testAllShipsSunken() {
   console.log(board.allShipsSunken());
 }
 
-function testPlayerCreation() {
+function testenemyCreation() {
   const axel = createPlayer();
   console.log(axel);
   const npc = createPlayer("computer");
